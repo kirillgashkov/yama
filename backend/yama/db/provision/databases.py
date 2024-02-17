d850c1f7519a98dbbf7ed2be9f06eef872436cd0
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-17 23:43:11 +0300

    Derive migrate DSN from SQLAlchemy DSN

--- backend/yama/db/provision/databases.py
+++ backend/yama/db/provision/databases.py
@@ -1,7 +1,8 @@
 import asyncio
 from pathlib import Path
+from urllib.parse import urlencode, urlunsplit
 
-from sqlalchemy import text, Dialect
+from sqlalchemy import URL, Dialect, text
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 
@@ -13,6 +14,32 @@ def _quoted_identifier(identifier: str, *, dialect: Dialect) -> str:
     return dialect.identifier_preparer.quote(identifier)
 
 
+# FIXME: Make SSL configurable
+# HACK: `host` and `port` aren't validated
+def _migrate_connection_url_from_sqlalchemy_connection_url(url: URL) -> str:
+    if url.username is None:
+        raise ValueError("Connection URL must contain a username")
+    if url.password is None:
+        raise ValueError("Connection URL must contain a password")
+    if url.database is None:
+        raise ValueError("Connection URL must contain a database name")
+    if url.host is None:
+        raise ValueError("Connection URL must contain a host")
+    if url.port is None:
+        raise ValueError("Connection URL must contain a port")
+
+    query = {
+        "user": url.username,
+        "password": url.password,
+        "dbname": url.database,
+        "sslmode": "disable",
+    }
+
+    return urlunsplit(
+        ("postgresql", f"{url.host}:{url.port}", "/", urlencode(query), "")
+    )
+
+
 async def database_exists(conn: AsyncConnection, *, database_name: str) -> bool:
     result = await conn.execute(
         text("SELECT 1 FROM pg_database WHERE datname = :database_name"),
@@ -21,29 +48,30 @@ async def database_exists(conn: AsyncConnection, *, database_name: str) -> bool:
     return (await result.first()) is not None
 
 
-async def create_database(
-    autocommit_conn: AsyncConnection, *, database_name: str
-) -> None:
+async def create_database(conn: AsyncConnection, *, database_name: str) -> None:
     quoted_database_name = _quoted_identifier(
-        database_name, dialect=autocommit_conn.engine.dialect
+        database_name, dialect=conn.engine.dialect
     )
-    await autocommit_conn.execute(text(f"CREATE DATABASE {quoted_database_name}"))
+    await conn.execute(text(f"CREATE DATABASE {quoted_database_name}"))
 
 
-async def drop_database(
-    autocommit_conn: AsyncConnection, *, database_name: str
-) -> None:
+async def drop_database(conn: AsyncConnection, *, database_name: str) -> None:
     quoted_database_name = _quoted_identifier(
-        database_name, dialect=autocommit_conn.engine.dialect
+        database_name, dialect=conn.engine.dialect
     )
-    await autocommit_conn.execute(text(f"DROP DATABASE {quoted_database_name}"))
+    await conn.execute(text(f"DROP DATABASE {quoted_database_name}"))
 
 
 async def upgrade_database(
+    conn: AsyncConnection,
+    *,
     migrate_executable: Path,
-    migrate_connection_url: str,
     migrate_migrations_dir: Path,
 ) -> None:
+    migrate_connection_url = _migrate_connection_url_from_sqlalchemy_connection_url(
+        conn.engine.url
+    )
+
     process = await asyncio.create_subprocess_exec(
         str(migrate_executable),
         "-database",
