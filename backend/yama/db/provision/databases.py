5edce2af52c93003f1e598a324d45c0ae0bbddcb
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-18 00:21:33 +0300

    Fix 'database_exists()', 'upgrade_database()'

--- backend/yama/db/provision/databases.py
+++ backend/yama/db/provision/databases.py
@@ -10,34 +10,35 @@ class DatabaseProvisionError(Exception):
     ...
 
 
-def _quoted_identifier(identifier: str, *, dialect: Dialect) -> str:
+def _quoted_identifier(identifier: str, dialect: Dialect) -> str:
     return dialect.identifier_preparer.quote(identifier)
 
 
 # FIXME: Make SSL configurable
 # HACK: `host` and `port` aren't validated
-def _migrate_connection_url_from_sqlalchemy_connection_url(url: URL) -> str:
-    if url.username is None:
-        raise ValueError("Connection URL must contain a username")
-    if url.password is None:
-        raise ValueError("Connection URL must contain a password")
-    if url.database is None:
-        raise ValueError("Connection URL must contain a database name")
-    if url.host is None:
-        raise ValueError("Connection URL must contain a host")
-    if url.port is None:
-        raise ValueError("Connection URL must contain a port")
+def _migrate_connection_url(database_name: str, sqlalchemy_connection_url: URL) -> str:
+    username = sqlalchemy_connection_url.username
+    password = sqlalchemy_connection_url.password
+    host = sqlalchemy_connection_url.host
+    port = sqlalchemy_connection_url.port
+
+    if username is None:
+        raise ValueError("Username is required")
+    if password is None:
+        raise ValueError("Password is required")
+    if host is None:
+        raise ValueError("Host is required")
+    if port is None:
+        raise ValueError("Port is required")
 
     query = {
-        "user": url.username,
-        "password": url.password,
-        "dbname": url.database,
+        "user": username,
+        "password": password,
+        "dbname": database_name,
         "sslmode": "disable",
     }
 
-    return urlunsplit(
-        ("postgresql", f"{url.host}:{url.port}", "/", urlencode(query), "")
-    )
+    return urlunsplit(("postgresql", f"{host}:{port}", "/", urlencode(query), ""))
 
 
 async def database_exists(conn: AsyncConnection, *, database_name: str) -> bool:
@@ -45,33 +46,25 @@ async def database_exists(conn: AsyncConnection, *, database_name: str) -> bool:
         text("SELECT 1 FROM pg_database WHERE datname = :database_name"),
         {"database_name": database_name},
     )
-    return (await result.first()) is not None
+    return result.first() is not None
 
 
 async def create_database(conn: AsyncConnection, *, database_name: str) -> None:
-    quoted_database_name = _quoted_identifier(
-        database_name, dialect=conn.engine.dialect
-    )
+    quoted_database_name = _quoted_identifier(database_name, conn.engine.dialect)
     await conn.execute(text(f"CREATE DATABASE {quoted_database_name}"))
 
 
 async def drop_database(conn: AsyncConnection, *, database_name: str) -> None:
-    quoted_database_name = _quoted_identifier(
-        database_name, dialect=conn.engine.dialect
-    )
+    quoted_database_name = _quoted_identifier(database_name, conn.engine.dialect)
     await conn.execute(text(f"DROP DATABASE {quoted_database_name}"))
 
 
 async def upgrade_database(
-    conn: AsyncConnection,
     *,
     migrate_executable: Path,
+    migrate_connection_url: str,
     migrate_migrations_dir: Path,
 ) -> None:
-    migrate_connection_url = _migrate_connection_url_from_sqlalchemy_connection_url(
-        conn.engine.url
-    )
-
     process = await asyncio.create_subprocess_exec(
         str(migrate_executable),
         "-database",
@@ -96,9 +89,11 @@ async def setup_database(
     if not await database_exists(conn, database_name=database_name):
         await create_database(conn, database_name=database_name)
 
+    migrate_connection_url = _migrate_connection_url(database_name, conn.engine.url)
+
     await upgrade_database(
-        conn,
         migrate_executable=migrate_executable,
+        migrate_connection_url=migrate_connection_url,
         migrate_migrations_dir=migrate_migrations_dir,
     )
 
