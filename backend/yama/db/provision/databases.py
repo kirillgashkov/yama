a1a6695c1bee79cb1b64078f0597c4a3458d68aa
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-18 16:19:22 +0300

    Add 'setup_database()', 'teardown_database()'

--- backend/yama/db/provision/databases.py
+++ backend/yama/db/provision/databases.py
@@ -1,7 +1,8 @@
 import asyncio
 from pathlib import Path
+from urllib.parse import urlencode, urlunsplit
 
-from sqlalchemy import Dialect, text
+from sqlalchemy import URL, Dialect, text
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 
@@ -49,3 +50,47 @@ async def upgrade_database(
 
     if process.returncode != 0:
         raise DatabaseProvisionError("Database migration failed")
+
+
+def _migrate_connection_url(database: str, sqlalchemy_connection_url: URL) -> str:
+    username = sqlalchemy_connection_url.username
+    password = sqlalchemy_connection_url.password
+    host = sqlalchemy_connection_url.host
+    port = sqlalchemy_connection_url.port
+
+    if username is None:
+        raise ValueError("Username is required")
+    if password is None:
+        raise ValueError("Password is required")
+    if host is None:
+        raise ValueError("Host is required")
+    if port is None:
+        raise ValueError("Port is required")
+
+    query = {"user": username, "password": password, "dbname": database}
+
+    return urlunsplit(("postgresql", f"{host}:{port}", "/", urlencode(query), ""))
+
+
+async def setup_database(
+    conn: AsyncConnection,
+    *,
+    database: str,
+    migrate_executable: Path,
+    migrate_migrations_dir: Path,
+) -> None:
+    if not await database_exists(conn, database=database):
+        await create_database(conn, database=database)
+
+    migrate_connection_url = _migrate_connection_url(database, conn.engine.url)
+
+    await upgrade_database(
+        migrate_executable=migrate_executable,
+        migrate_connection_url=migrate_connection_url,
+        migrate_migrations_dir=migrate_migrations_dir,
+    )
+
+
+async def teardown_database(conn: AsyncConnection, *, database: str) -> None:
+    if await database_exists(conn, database=database):
+        await drop_database(conn, database=database)
