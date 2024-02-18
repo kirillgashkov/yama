cee7aab6babd6d165df719b497d534cea78f3bc0
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-18 21:53:39 +0300

    Enable envvars for nested settings models

--- backend/yama/db/settings.py
+++ backend/yama/db/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ProvisionSettings(BaseSettings):
@@ -12,6 +12,8 @@ class ProvisionSettings(BaseSettings):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
+
     host: str
     port: int
     username: str
