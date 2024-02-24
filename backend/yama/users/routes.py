26f499766a5b63ffbdbb00c26cd505a8f3d3b26a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-24 14:11:50 +0300

    Add '/users' POST route

--- backend/yama/users/routes.py
+++ backend/yama/users/routes.py
@@ -1,11 +1,13 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 from yama.database.dependencies import get_connection
-from yama.users.models import User, UserOut
+from yama.security.utils import hash_password
+from yama.users.models import User, UserIn, UserOut
+from yama.users.utils import user_exists
 
 router = APIRouter()
 
@@ -17,3 +19,22 @@ async def get_users(
     statement = select(User.id, User.username)
     rows = (await connection.execute(statement)).mappings()
     return [UserOut(**row) for row in rows]
+
+
+@router.post("/users")
+async def create_user(
+    user_in: UserIn,
+    connection: Annotated[AsyncConnection, Depends(get_connection)],
+) -> UserOut:
+    if await user_exists(user_in.username, connection):
+        raise ValueError("User already exists")  # FIXME: Throw an HTTP exception
+
+    password_hash = hash_password(user_in.password)
+
+    statement = (
+        insert(User)
+        .values(username=user_in.username, password_hash=password_hash)
+        .returning(User.id, User.username)
+    )
+    row = (await connection.execute(statement)).mappings().one()
+    return UserOut(**row)
