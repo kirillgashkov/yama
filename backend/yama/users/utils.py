99767edc9fe7474fcae4c37370979a1f8ba4c7db
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-25 13:28:12 +0300

    Improve 'user_exists()'

--- backend/yama/users/utils.py
+++ backend/yama/users/utils.py
@@ -1,10 +1,12 @@
-from sqlalchemy import func, select
+from sqlalchemy import exists, func, select
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 from yama.users.models import User
 
 
 async def user_exists(username: str, connection: AsyncConnection) -> bool:
-    statement = select(User.id).where(func.lower(User.username) == func.lower(username))
+    statement = select(
+        exists().where(func.lower(User.username) == func.lower(username))
+    )
     row = await connection.execute(statement)
-    return bool(row.scalar())
+    return row.scalar_one()
