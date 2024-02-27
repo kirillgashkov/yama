82f540b3b21e37f1e33f5b3fb205fae5309d4be1
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-27 15:58:00 +0300

    Add 'is_password_valid()' mock

--- backend/yama/security/utils.py
+++ backend/yama/security/utils.py
@@ -1,3 +1,8 @@
 # FIXME: Use Argon2id
 def hash_password(password: str) -> str:
     return f"hash({password})"
+
+
+# FIXME: Use Argon2id
+def is_password_valid(password: str, password_hash: str) -> bool:
+    return hash_password(password) == password_hash
