892e59f562d161bd908a50cfbe17ba2dc7e32b03
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-27 16:07:04 +0300

    Add 'create_access_token()', 'is_access_token_valid()' mocks

--- backend/yama/security/utils.py
+++ backend/yama/security/utils.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+
+
 # FIXME: Use Argon2id
 def hash_password(password: str) -> str:
     return f"hash({password})"
@@ -6,3 +9,13 @@ def hash_password(password: str) -> str:
 # FIXME: Use Argon2id
 def is_password_valid(password: str, password_hash: str) -> bool:
     return hash_password(password) == password_hash
+
+
+# FIXME: Use JWT
+def create_access_token(user_id: UUID) -> str:
+    return f"access_token({user_id})"
+
+
+# FIXME: Use JWT
+def is_access_token_valid(access_token: str) -> bool:
+    return access_token.startswith("access_token(") and access_token.endswith(")")
