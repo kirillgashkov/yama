e27aca36cec4d82f266698b336c43f9c235cb269
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-28 13:32:51 +0300

    Add 'root_file_id' to 'files' settings

--- backend/yama/files/settings.py
+++ backend/yama/files/settings.py
@@ -1,8 +1,11 @@
+from uuid import UUID
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="yama_files_")
 
+    root_file_id: UUID
     max_chunk_size: int = 1024 * 1024 * 10  # 10 MiB
     max_file_size: int = 1024 * 1024 * 512  # 512 MiB
