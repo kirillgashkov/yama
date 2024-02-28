4e7c629c65acbdc2449830ee62c0f78c9aa420dc
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-28 14:18:44 +0300

    Add 'files_dir' to 'files' settings

--- backend/yama/files/settings.py
+++ backend/yama/files/settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from uuid import UUID
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -6,6 +7,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="yama_files_")
 
+    files_dir: Path
     root_file_id: UUID
     max_chunk_size: int = 1024 * 1024 * 10  # 10 MiB
     max_file_size: int = 1024 * 1024 * 512  # 512 MiB
