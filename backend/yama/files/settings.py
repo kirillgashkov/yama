b08380c3141248188712add81b8101a21744e13b
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-28 09:40:57 +0300

    Add 'max_chunk_size', 'max_file_size' to 'files' settings

---
+++ backend/yama/files/settings.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="yama_files_")
+
+    max_chunk_size: int = 1024 * 1024 * 10  # 10 MiB
+    max_file_size: int = 1024 * 1024 * 512  # 512 MiB
