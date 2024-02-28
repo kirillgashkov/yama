9c93c9a761efce7e0136ea95737bd55e961fe15c
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-28 08:37:50 +0300

    Add 'FileType', 'File', 'FileAncestorFileDescendant' models

---
+++ backend/yama/files/models.py
@@ -0,0 +1,32 @@
+from uuid import UUID
+
+from sqlalchemy import ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from yama.database.models import TableBase
+
+
+class FileType(TableBase):
+    __tablename__ = "file_types"
+
+    type: Mapped[str] = mapped_column(primary_key=True)
+
+
+class File(TableBase):
+    __tablename__ = "files"
+
+    id: Mapped[UUID] = mapped_column(
+        server_default=func.uuid_generate_v4(), primary_key=True
+    )
+    type: Mapped[str] = mapped_column(ForeignKey("file_types.type"))
+
+
+class FileAncestorFileDescendant(TableBase):
+    __tablename__ = "file_ancestors_file_descendants"
+
+    ancestor_id: Mapped[UUID] = mapped_column(ForeignKey("files.id"), primary_key=True)
+    descendant_id: Mapped[UUID] = mapped_column(
+        ForeignKey("files.id"), primary_key=True
+    )
+    descendant_path: Mapped[str]
+    depth: Mapped[int]
