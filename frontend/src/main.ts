f3fc9ae17d7481ec9bf0c28303c1d993a07f64d2
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-01 00:25:34 +0300

    Add style.css import to main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import { createApp } from "vue";
+import "@/style.css";
 import App from "@/App.vue";
+import { createApp } from "vue";
 
 createApp(App).mount("#app"); // eslint-disable-line @typescript-eslint/no-unsafe-argument -- https://github.com/vuejs/vue-eslint-parser/issues/104
