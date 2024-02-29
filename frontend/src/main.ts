e1774ca1782f269c5f52c7678e964c4b4848044e
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-29 17:53:00 +0300

    Ignore a ESLint error regarding 'App.vue' being 'any'

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 
-createApp(App).mount("#app");
+createApp(App).mount("#app"); // eslint-disable-line @typescript-eslint/no-unsafe-argument -- https://github.com/vuejs/vue-eslint-parser/issues/104
