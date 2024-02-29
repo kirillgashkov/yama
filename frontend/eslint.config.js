8815561465fa6bad342d808d1a2b8bea5cb5dee0
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-02-29 16:06:43 +0300

    Configure ESLint for Vue
    - 'eslint-plugin-vue' doesn't support flat configuration yet, therefore
      we rely on '@eslint/eslintrc' to use Vue's recommended configuration.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,24 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
 import js from "@eslint/js";
 import ts from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const eslintrc = new FlatCompat({
+  baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
+});
 
 export default ts.config(
   js.configs.recommended,
   ...ts.configs.recommendedTypeChecked,
+  ...eslintrc.extends("plugin:vue/vue3-recommended"), // Depends on `eslint-plugin-vue`
   {
     languageOptions: {
+      parser: vueParser,
       parserOptions: {
+        parser: ts.parser,
         EXPERIMENTAL_useProjectService: true,
       },
     },
