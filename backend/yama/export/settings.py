f9fee70e2d6a69f5bc6473f134b82fe19ed2481a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-06 12:44:27 +0300

    Add 'latexmk_executable', 'pandoc_executable' settings to 'export'

---
+++ backend/yama/export/settings.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="yama_export_")
+
+    latexmk_executable: Path = Path("latexmk")
+    pandoc_executable: Path = Path("pandoc")
