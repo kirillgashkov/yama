41d9b8d18a31f8691a10143c3698854955aeec57
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-06 09:46:12 +0300

    Add route mocks to 'files'

--- backend/yama/files/routes.py
+++ backend/yama/files/routes.py
@@ -1,3 +1,23 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+
+@router.get("/files/{parent_path:path}")
+async def create_file() -> None:
+    ...
+
+
+@router.get("/files/{path:path}")
+async def read_file() -> None:
+    ...
+
+
+@router.get("/files/{path:path}")
+async def update_file() -> None:
+    ...
+
+
+@router.get("/files/{path:path}")
+async def delete_file() -> None:
+    ...
