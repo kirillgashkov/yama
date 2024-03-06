97488c3a6aad3ee61bfa94290fd6fe76f9de6427
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-06 09:52:58 +0300

    Mock POST /files/{parent_path} parameters

--- backend/yama/files/routes.py
+++ backend/yama/files/routes.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from typing import Annotated
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, File, Form, UploadFile
+from sqlalchemy.ext.asyncio import AsyncConnection
+
+from yama.database.dependencies import get_connection
+from yama.files.models import FileTypeEnum
+from yama.security.dependencies import get_current_user_id
 
 router = APIRouter()
 
 
 @router.get("/files/{parent_path:path}")
-async def create_file() -> None:
+async def create_file(
+    parent_path: str,
+    /,
+    name: Annotated[str, Form()],
+    type: Annotated[FileTypeEnum, Form()],
+    content: Annotated[UploadFile | None, File()] = None,
+    *,
+    connection: Annotated[AsyncConnection, Depends(get_connection)],
+    current_user_id: Annotated[UUID, Depends(get_current_user_id)],
+) -> ...:
     ...
 
 
