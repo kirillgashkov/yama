ecc97921e30b3f9bbd363af30972fa477fc8ca47
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-03-07 12:27:59 +0300

    Add 'FileName', 'FilePath' types to 'files'

--- backend/yama/files/models.py
+++ backend/yama/files/models.py
@@ -1,16 +1,65 @@
 from enum import Enum
+from pathlib import PurePosixPath
+from typing import Annotated, Any, TypeAlias
 from uuid import UUID
 
+from pydantic import AfterValidator, ValidatorFunctionWrapHandler, WrapValidator
 from sqlalchemy import ForeignKey, String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from yama.database.models import TableBase
+from yama.files.constants import MAX_FILE_NAME_LENGTH, MAX_FILE_PATH_LENGTH
 from yama.model.models import ModelBase
+
+
+def check_file_name(name: str) -> str:
+    assert len(name.encode()) <= MAX_FILE_NAME_LENGTH, "File name is too long"
+    assert name.isprintable(), "File name contains non-printable characters"
+    assert "/" not in name, "File name contains '/'"
+    return name
+
+
+def check_file_path(
+    path_str: Any, handler: ValidatorFunctionWrapHandler
+) -> PurePosixPath:
+    assert isinstance(path_str, str), "File path is not a string"
+    assert len(path_str.encode()) <= MAX_FILE_PATH_LENGTH, "File path is too long"
+
+    path = handler(path_str)
+
+    if not isinstance(path, PurePosixPath):
+        raise RuntimeError("File path is not a PurePosixPath")
+
+    if path.is_absolute():
+        names = path.parts[1:]
+    else:
+        names = path.parts
+
+    for name in names:
+        check_file_name(name)
+
+    return path
+
+
+FileName: TypeAlias = Annotated[str, AfterValidator(check_file_name)]
+FilePath: TypeAlias = Annotated[PurePosixPath, WrapValidator(check_file_path)]
 
 
 class FileTypeEnum(str, Enum):
     REGULAR = "regular"
     DIRECTORY = "directory"
+
+
+class FileIn(ModelBase):
+    parent_path: FilePath
+    name: FileName
+    type: FileTypeEnum
+
+
+class FileOut(ModelBase):
+    path: FilePath
+    id: UUID
+    type: FileTypeEnum
 
 
 class FileType(TableBase):
@@ -25,7 +74,9 @@ class File(TableBase):
     id: Mapped[UUID] = mapped_column(
         server_default=func.uuid_generate_v4(), primary_key=True
     )
-    type: Mapped[str] = mapped_column(ForeignKey("file_types.type"))
+    type: Mapped[str] = mapped_column(
+        ForeignKey("file_types.type")
+    )  # FIXME: Mapped[FileTypeEnum]
 
 
 class FileAncestorFileDescendant(TableBase):
@@ -37,15 +88,3 @@ class FileAncestorFileDescendant(TableBase):
     )
     descendant_path: Mapped[str]
     depth: Mapped[int]
-
-
-class FileIn(ModelBase):
-    parent_path: str
-    name: str
-    type: FileTypeEnum
-
-
-class FileOut(ModelBase):
-    path: str
-    id: UUID
-    type: FileTypeEnum
