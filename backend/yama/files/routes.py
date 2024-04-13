941cf2c5102c8cbec8d2d2d470308a6cb68b2b99
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-13 13:59:02 +0300

    Update route parameters in 'files'

--- backend/yama/files/routes.py
+++ backend/yama/files/routes.py
@@ -1,11 +1,13 @@
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, File, Form, UploadFile
+from fastapi import APIRouter, Depends, File, Form, Query, UploadFile
 from sqlalchemy.ext.asyncio import AsyncConnection
 
 from yama.database.dependencies import get_connection
+from yama.files.dependencies import get_settings
 from yama.files.models import FileName, FilePath, FileTypeEnum
+from yama.files.settings import Settings
 from yama.security.dependencies import get_current_user_id, get_current_user_id_or_none
 
 router = APIRouter()
@@ -13,44 +15,54 @@ router = APIRouter()
 
 @router.post("/files/{parent_path:path}")
 async def create_file(
-    parent_path: FilePath,
     *,
+    parent_path: FilePath,
     name: Annotated[FileName, Form()],
     type: Annotated[FileTypeEnum, Form()],
     content: Annotated[UploadFile | None, File()] = None,
+    working_dir: Annotated[UUID | None, Query()] = None,
+    user_id: Annotated[UUID, Depends(get_current_user_id)],
+    settings: Annotated[Settings, Depends(get_settings)],
     connection: Annotated[AsyncConnection, Depends(get_connection)],
-    current_user_id: Annotated[UUID, Depends(get_current_user_id)],
-) -> None:  # FIXME: Return type
+) -> None:
     ...
 
 
-@router.get("/files/{path:path}")
-async def read_file(
-    path: FilePath,
+@router.post("/files/{path:path}")
+async def get_file(
     *,
+    path: FilePath,
+    type: Annotated[FileTypeEnum | None, Query()] = None,
+    working_dir: Annotated[UUID | None, Query()] = None,
+    user_id: Annotated[UUID | None, Depends(get_current_user_id_or_none)],
+    settings: Annotated[Settings, Depends(get_settings)],
     connection: Annotated[AsyncConnection, Depends(get_connection)],
-    current_user_id: Annotated[UUID | None, Depends(get_current_user_id_or_none)],
-) -> None:  # FIXME: Return type
+) -> None:
     ...
 
 
 @router.put("/files/{path:path}")
-async def update_file(
-    path: FilePath,
+async def create_or_update_file(
     *,
-    name: Annotated[FileName, Form()],
+    path: FilePath,
+    type: Annotated[FileTypeEnum, Form()],
     content: Annotated[UploadFile | None, File()] = None,
+    working_dir: Annotated[UUID | None, Query()] = None,
+    user_id: Annotated[UUID, Depends(get_current_user_id)],
+    settings: Annotated[Settings, Depends(get_settings)],
     connection: Annotated[AsyncConnection, Depends(get_connection)],
-    current_user_id: Annotated[UUID, Depends(get_current_user_id)],
-) -> None:  # FIXME: Return type
+) -> None:
     ...
 
 
 @router.delete("/files/{path:path}")
 async def delete_file(
-    path: FilePath,
     *,
+    path: FilePath,
+    type: Annotated[FileTypeEnum | None, Query()] = None,
+    working_dir: Annotated[UUID | None, Query()] = None,
+    user_id: Annotated[UUID, Depends(get_current_user_id)],
+    settings: Annotated[Settings, Depends(get_settings)],
     connection: Annotated[AsyncConnection, Depends(get_connection)],
-    current_user_id: Annotated[UUID, Depends(get_current_user_id)],
-) -> None:  # FIXME: Return type
+) -> None:
     ...
