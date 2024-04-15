9f38f234f4a17d4b5039cdedda25d176faebf25f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-15 14:52:56 +0300

    Add file share tables in migrations

--- backend/yama/database/provision/migrations/0_init.up.sql
+++ backend/yama/database/provision/migrations/0_init.up.sql
@@ -49,4 +49,23 @@ CREATE TABLE IF NOT EXISTS file_ancestors_file_descendants (
     FOREIGN KEY (descendant_id) REFERENCES files (id)
 );
 
+CREATE TABLE IF NOT EXISTS file_share_types (
+    type varchar NOT NULL,
+    PRIMARY KEY (type)
+);
+INSERT INTO file_share_types (type) VALUES ('read'), ('write'), ('share');
+
+CREATE TABLE IF NOT EXISTS file_shares (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    type varchar NOT NULL,
+    file_id uuid NOT NULL,
+    user_id uuid NOT NULL,
+    created_by uuid NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (type) REFERENCES file_types (type),
+    FOREIGN KEY (file_id) REFERENCES files (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (created_by) REFERENCES users (id)
+);
+
 COMMIT;
