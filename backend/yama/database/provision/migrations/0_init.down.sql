40597653c78ac566c28ceff9deb6c07c0f233b3a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-15 14:56:48 +0300

    Update the down migration

--- backend/yama/database/provision/migrations/0_init.down.sql
+++ backend/yama/database/provision/migrations/0_init.down.sql
@@ -1,12 +1,20 @@
 BEGIN;
 
+DROP TABLE IF EXISTS file_shares;
+
+DROP TABLE IF EXISTS file_share_types;
+
 DROP TABLE IF EXISTS file_ancestors_file_descendants;
 
 DROP TABLE IF EXISTS files;
 
 DROP TABLE IF EXISTS file_types;
 
+DROP TABLE IF EXISTS user_ancestors_user_descendants;
+
 DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS user_types;
 
 DROP EXTENSION IF EXISTS "uuid-ossp";
 
