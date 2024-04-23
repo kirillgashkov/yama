d427a0f0d8e2da0afdc17b63b1645d7cb5463fb9
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-23 23:27:24 +0300

    Rename 'user' settings's env prefix

--- backend/yama/user/settings.py
+++ backend/yama/user/settings.py
@@ -4,6 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="yama_users_")
+    model_config = SettingsConfigDict(env_prefix="yama_user_")
 
     public_user_id: UUID
