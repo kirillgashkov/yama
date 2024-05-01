a320e2d7e862bad0771fad3ba6abd6f96921058c
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-01 17:49:54 +0300

    Fix read_regular_content in file.driver

--- backend/yama/file/driver/utils.py
+++ backend/yama/file/driver/utils.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
 from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 from pathlib import Path
-from typing import Protocol
+from typing import Protocol, override
 from uuid import UUID
 
 import aiofiles
@@ -10,40 +11,44 @@ import aiofiles.os
 
 # Satisfied by fastapi.UploadFile and aiofiles's files
 class AsyncReadable(Protocol):
-    async def read(self, size: int = ..., /) -> bytes:
-        ...
+    async def read(self, size: int = ..., /) -> bytes: ...
 
 
 class Driver(ABC):
     @abstractmethod
-    def read_regular_content(self, id_: UUID, /) -> AsyncIterator[AsyncReadable]:
-        ...
+    @asynccontextmanager
+    def read_regular_content(self, id_: UUID, /) -> AsyncIterator[AsyncReadable]: ...
 
     @abstractmethod
     async def write_regular_content(
         self, content_stream: AsyncReadable, id_: UUID, /
-    ) -> int:
-        ...
+    ) -> int: ...
 
     @abstractmethod
-    async def remove_regular_content(self, id_: UUID, /) -> None:
-        ...
+    async def remove_regular_content(self, id_: UUID, /) -> None: ...
 
 
 class FileSystemDriver(Driver):
     def __init__(
         self, /, *, chunk_size: int, file_system_dir: Path, max_file_size: int
     ) -> None:
+        super().__init__()
         self.chunk_size = chunk_size
         self.file_system_dir = file_system_dir
         self.max_file_size = max_file_size
 
+    @override
+    @asynccontextmanager
     async def read_regular_content(self, id_: UUID, /) -> AsyncIterator[AsyncReadable]:
         path = _id_to_path(id_, file_system_dir=self.file_system_dir)
 
-        async with aiofiles.open(path, "rb") as f:
-            yield f
+        try:
+            async with aiofiles.open(path, "rb") as f:
+                yield f
+        except FileNotFoundError:
+            raise DriverFileNotFound(id_)
 
+    @override
     async def write_regular_content(
         self, content_stream: AsyncReadable, id_: UUID, /
     ) -> int:
@@ -63,14 +68,15 @@ class FileSystemDriver(Driver):
                     if file_size > self.max_file_size:
                         raise DriverFileTooLargeError()
 
-                    await f.write(chunk)
+                    _ = await f.write(chunk)
 
-            incomplete_path.rename(complete_path)
+            _ = incomplete_path.rename(complete_path)
         finally:
             incomplete_path.unlink(missing_ok=True)
 
         return file_size
 
+    @override
     async def remove_regular_content(self, id_: UUID, /) -> None:
         path = _id_to_path(id_, file_system_dir=self.file_system_dir)
 
@@ -88,5 +94,14 @@ def _id_to_incomplete_path(id_: UUID, /, *, file_system_dir: Path) -> Path:
     return file_system_dir / (id_.hex + ".incomplete")
 
 
-class DriverFileTooLargeError(Exception):
-    ...
+class DriverFileTooLargeError(Exception): ...
+
+
+class DriverFileNotFound(Exception):
+    def __init__(self, id_: UUID, /) -> None:
+        super().__init__()
+        self.id_ = id_
+
+    @override
+    def __str__(self) -> str:
+        return f"'{self.id_}'"
