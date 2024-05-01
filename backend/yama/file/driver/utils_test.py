99e01616aa321757841b098be9b605affdaa2bb9
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-01 19:56:04 +0300

    Merge read_regular_content tests

--- backend/yama/file/driver/utils_test.py
+++ backend/yama/file/driver/utils_test.py
@@ -24,17 +24,8 @@ async def test_file_system_driver_read_regular_content(*, tmp_path: Path) -> Non
         UUID("42bd9c32-1c96-485f-af69-b48536bc3c4a")
     ) as f:
         content = await f.read()
-
     assert content == b"# Foo\n\nBar.\n"
 
-
-async def test_file_system_driver_read_regular_content__file_not_found(
-    *, tmp_path: Path
-) -> None:
-    """Tests the FileSystemDriver.read_regular_content method where the file is missing."""
-    driver = FileSystemDriver(
-        chunk_size=64, file_system_dir=tmp_path, max_file_size=512
-    )
     with pytest.raises(DriverFileNotFound):
         async with driver.read_regular_content(
             UUID("00bd9c32-1c96-485f-af69-b48536bc3c4a")
