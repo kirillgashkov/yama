490b3e20d4cdc5cce1f1317ab367cd110fd25a55
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-01 19:44:55 +0300

    Fix a read_regular_content test to use correct path

--- backend/yama/file/driver/utils_test.py
+++ backend/yama/file/driver/utils_test.py
@@ -12,9 +12,7 @@ async def test_file_system_driver_read_regular_content(*, tmp_path: Path) -> Non
     driver = FileSystemDriver(
         chunk_size=64, file_system_dir=tmp_path, max_file_size=512
     )
-    async with aiofiles.open(
-        tmp_path / "42bd9c32-1c96-485f-af69-b48536bc3c4a", "wb"
-    ) as f:
+    async with aiofiles.open(tmp_path / "42bd9c321c96485faf69b48536bc3c4a", "wb") as f:
         _ = await f.write(b"# Foo\n\nBar.\n")
 
     async with driver.read_regular_content(
