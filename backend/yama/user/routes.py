1e3cfd843e7e63dd00bd5f4f568ec279b023a1b4
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-03 21:52:07 +0300

    Clean up

--- backend/yama/user/routes.py
+++ backend/yama/user/routes.py
@@ -14,6 +14,33 @@ from yama.user.utils import user_exists
 router = APIRouter()
 
 
+@router.post("/users")
+async def create_user(
+    *,
+    user_create_in: UserCreateIn,
+    connection: Annotated[AsyncConnection, Depends(get_connection)],
+) -> UserOut:
+    if await user_exists(handle=user_create_in.handle, connection=connection):
+        raise HTTPException(status_code=400, detail="User already exists.")
+
+    password_hash = hash_password(user_create_in.password)
+
+    query = (
+        insert(UserDb)
+        .values(
+            type=user_create_in.type.value,
+            handle=user_create_in.handle,
+            password_hash=password_hash,
+        )
+        .returning(UserDb)
+    )
+    row = (await connection.execute(query)).mappings().one()
+    user_db = UserDb(**row)
+    await connection.commit()
+
+    return _user_db_to_user_out(user_db)
+
+
 @router.get("/users/current")
 async def read_current_user(
     *,
@@ -53,33 +80,6 @@ async def read_users(
     users_db = [UserDb(**row) for row in rows]
 
     return [_user_db_to_user_out(u) for u in users_db]
-
-
-@router.post("/users")
-async def create_user(
-    *,
-    user_create_in: UserCreateIn,
-    connection: Annotated[AsyncConnection, Depends(get_connection)],
-) -> UserOut:
-    if await user_exists(handle=user_create_in.handle, connection=connection):
-        raise HTTPException(status_code=400, detail="User already exists.")
-
-    password_hash = hash_password(user_create_in.password)
-
-    query = (
-        insert(UserDb)
-        .values(
-            type=user_create_in.type.value,
-            handle=user_create_in.handle,
-            password_hash=password_hash,
-        )
-        .returning(UserDb)
-    )
-    row = (await connection.execute(query)).mappings().one()
-    user_db = UserDb(**row)
-    await connection.commit()
-
-    return _user_db_to_user_out(user_db)
 
 
 def _user_db_to_user_out(u: UserDb, /) -> UserOut:
