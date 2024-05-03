5b949c243254180ca9a007a01144eff296b57707
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-04 00:47:15 +0300

    Fix _make_regular_content_url to include /.

--- backend/yama/file/routes.py
+++ backend/yama/file/routes.py
@@ -222,6 +222,6 @@ def _make_regular_content_url(
     files_base_url: str,
 ) -> str:
     scheme, netloc, files_base_path, _, _ = urlsplit(files_base_url)
-    path = str(PurePosixPath(files_base_path) / ".")
+    path = str(PurePosixPath(files_base_path)) + "/."
     query = urlencode({"regular_content": True, "working_file_id": str(id_)})
     return urlunsplit((scheme, netloc, path, query, ""))
