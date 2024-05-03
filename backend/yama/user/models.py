c5e351096daf09eb7a7ec8b95d4550f6d3a257e6
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-03 22:32:39 +0300

    Add user handle validation

--- backend/yama/user/models.py
+++ backend/yama/user/models.py
@@ -1,12 +1,26 @@
 from enum import Enum
-from typing import Literal
+from typing import Annotated, Literal, TypeAlias
 from uuid import UUID
 
+from pydantic import AfterValidator
 from sqlalchemy import ForeignKey, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from yama.database.models import TableBase
 from yama.model.models import ModelBase
+from yama.user.settings import MAX_HANDLE_LENGTH, MIN_HANDLE_LENGTH
+
+
+def _check_handle(handle: str, /) -> str:
+    assert len(handle.encode()) >= MIN_HANDLE_LENGTH, "Handle is too short."
+    assert len(handle.encode()) <= MAX_HANDLE_LENGTH, "Handle is too long."
+    assert handle.isprintable(), "Handle contains non-printable characters."
+    assert "/" not in handle, 'Handle contains "/".'
+    assert handle != "current", 'Handle "current" is reserved.'
+    return handle
+
+
+Handle: TypeAlias = Annotated[str, AfterValidator(_check_handle)]
 
 
 class UserType(str, Enum):
@@ -17,12 +31,12 @@ class UserType(str, Enum):
 class UserOut(ModelBase):
     id: UUID
     type: UserType
-    handle: str
+    handle: Handle
 
 
 class UserCreateIn(ModelBase):
     type: Literal[UserType.USER]
-    handle: str
+    handle: Handle
     password: str
 
 
--- backend/yama/user/settings.py
+++ backend/yama/user/settings.py
@@ -2,6 +2,9 @@ from uuid import UUID
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+MIN_HANDLE_LENGTH = 1
+MAX_HANDLE_LENGTH = 255
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="yama_user__")
