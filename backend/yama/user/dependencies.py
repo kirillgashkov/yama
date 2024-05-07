d70d9a95fb268db0141021b274a7580afa198c55
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-07 09:32:11 +0300

    Clean up user dependencies

--- backend/yama/user/dependencies.py
+++ backend/yama/user/dependencies.py
@@ -9,19 +9,16 @@ from yama.user.settings import Settings
 from yama.user.utils import get_user_id_from_access_token, is_access_token_valid
 
 
-# `get_settings()` is a lifetime dependency that provides
-# `Settings` created by the lifespan
-async def get_settings(request: Request) -> Settings:
+# get_settings is a lifetime dependency that provides Settings created by the lifespan.
+async def get_settings(*, request: Request) -> Settings:
     return request.state.user_settings  # type: ignore[no-any-return]
 
 
-get_oauth2_token = OAuth2PasswordBearer(tokenUrl="/auth/token")
-get_oauth2_token_or_none = OAuth2PasswordBearer(
-    tokenUrl="/auth/token", auto_error=False
-)
+_get_oauth2_token = OAuth2PasswordBearer(tokenUrl="/auth")
+_get_oauth2_token_or_none = OAuth2PasswordBearer(tokenUrl="/auth", auto_error=False)
 
 
-def get_current_user_id(token: Annotated[str, Depends(get_oauth2_token)]) -> UUID:
+def get_current_user_id(*, token: Annotated[str, Depends(_get_oauth2_token)]) -> UUID:
     if not is_access_token_valid(token):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -32,7 +29,8 @@ def get_current_user_id(token: Annotated[str, Depends(get_oauth2_token)]) -> UUI
 
 
 def get_current_user_id_or_none(
-    token: Annotated[str | None, Depends(get_oauth2_token_or_none)],
+    *,
+    token: Annotated[str | None, Depends(_get_oauth2_token_or_none)],
 ) -> UUID | None:
     if token is None or not is_access_token_valid(token):
         return None
