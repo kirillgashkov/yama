baa6f8359b45c09ad77a2cebef356f8db735cef4
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-16 10:42:42 +0300

    Mark function.execute as not implemented yet

--- backend/yama/function/_function.py
+++ backend/yama/function/_function.py
@@ -1,9 +1,5 @@
-import asyncio
-import sys
 from dataclasses import dataclass
 from pathlib import Path
-
-import aiofiles
 
 from yama.model.models import ModelBase
 
@@ -33,22 +29,23 @@ async def execute(
     input_files: list[tuple[Path, bytes]],
     output_paths: list[Path],
 ) -> list[tuple[Path, bytes]]:
-    for file_inout in function_in.files:
-        async with aiofiles.open(file_inout.path, "wb") as f:
-            await f.write(file_inout.content)
-
-    process = await asyncio.create_subprocess_exec(
-        *command,
-        stdin=sys.stdin.buffer,
-        stdout=sys.stdout.buffer,
-        stderr=sys.stderr.buffer,
-    )
-    exit_code = await process.wait()
-    if exit_code != 0:
-        raise RuntimeError(f"Process exited with code {exit_code}")
-
-    files = []
-    for file_inout in function_in.files:
-        async with aiofiles.open(file_inout.path, "rb") as f:
-            content = await f.read()
-        files.append(FileInout(path=file_inout.path, content=content))
+    # for file_inout in function_in.files:
+    #     async with aiofiles.open(file_inout.path, "wb") as f:
+    #         await f.write(file_inout.content)
+    #
+    # process = await asyncio.create_subprocess_exec(
+    #     *command,
+    #     stdin=sys.stdin.buffer,
+    #     stdout=sys.stdout.buffer,
+    #     stderr=sys.stderr.buffer,
+    # )
+    # exit_code = await process.wait()
+    # if exit_code != 0:
+    #     raise RuntimeError(f"Process exited with code {exit_code}")
+    #
+    # files = []
+    # for file_inout in function_in.files:
+    #     async with aiofiles.open(file_inout.path, "rb") as f:
+    #         content = await f.read()
+    #     files.append(FileInout(path=file_inout.path, content=content))
+    ...
