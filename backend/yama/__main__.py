8af6d97f3893b53cc954e7fc1ecb4e43ea7bb54a
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-16 17:50:41 +0300

    Clean up database utility function names

--- backend/yama/__main__.py
+++ backend/yama/__main__.py
@@ -7,7 +7,7 @@ from typer import Typer
 from yama import api, database, function
 from yama.database import provision
 from yama.database.provision import setup_database, teardown_database
-from yama.database.utils import sqlalchemy_async_connection
+from yama.database.utils import make_sqlalchemy_async_connection
 
 app = Typer()
 database_app = Typer()
@@ -29,7 +29,7 @@ def handle_database_up() -> None:
         database_settings = database.Config()  # pyright: ignore[reportCallIssue]
         database_provision_settings = provision.Config()  # pyright: ignore[reportCallIssue]
 
-        async with sqlalchemy_async_connection(
+        async with make_sqlalchemy_async_connection(
             host=database_settings.host,
             port=database_settings.port,
             username=database_provision_settings.username,
@@ -53,7 +53,7 @@ def handle_database_down() -> None:
         database_settings = database.Config()  # pyright: ignore[reportCallIssue]
         database_provision_settings = provision.Config()  # pyright: ignore[reportCallIssue]
 
-        async with sqlalchemy_async_connection(
+        async with make_sqlalchemy_async_connection(
             host=database_settings.host,
             port=database_settings.port,
             username=database_provision_settings.username,
--- backend/yama/database/utils.py
+++ backend/yama/database/utils.py
@@ -5,24 +5,11 @@ from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine, create_async_engine
 
 
-def sqlalchemy_connection_url(
-    *, host: str, port: int, username: str, password: str, database: str
-) -> URL:
-    return URL.create(
-        drivername="postgresql+asyncpg",
-        username=username,
-        password=password,
-        host=host,
-        port=port,
-        database=database,
-    )
-
-
 @asynccontextmanager
-async def sqlalchemy_async_engine(
+async def make_sqlalchemy_async_engine(
     *, host: str, port: int, username: str, password: str, database: str
 ) -> AsyncIterator[AsyncEngine]:
-    url = sqlalchemy_connection_url(
+    url = _make_sqlalchemy_connection_url(
         host=host, port=port, username=username, password=password, database=database
     )
     engine = create_async_engine(url)
@@ -33,11 +20,24 @@ async def sqlalchemy_async_engine(
 
 
 @asynccontextmanager
-async def sqlalchemy_async_connection(
+async def make_sqlalchemy_async_connection(
     *, host: str, port: int, username: str, password: str, database: str
 ) -> AsyncIterator[AsyncConnection]:
-    async with sqlalchemy_async_engine(
+    async with make_sqlalchemy_async_engine(
         host=host, port=port, username=username, password=password, database=database
     ) as engine:
         async with engine.connect() as conn:
             yield conn
+
+
+def _make_sqlalchemy_connection_url(
+    *, host: str, port: int, username: str, password: str, database: str
+) -> URL:
+    return URL.create(
+        drivername="postgresql+asyncpg",
+        username=username,
+        password=password,
+        host=host,
+        port=port,
+        database=database,
+    )
