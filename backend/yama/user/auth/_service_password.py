debf1604c29f501e550d05fe9ab8da761fd22b6f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-18 19:21:57 +0300

    Rehash password on password sign in

--- backend/yama/user/auth/_service_password.py
+++ backend/yama/user/auth/_service_password.py
@@ -1,10 +1,14 @@
 from fastapi import HTTPException
-from sqlalchemy import func, select
+from sqlalchemy import func, select, update
 from sqlalchemy.ext.asyncio import AsyncConnection
 from starlette.status import HTTP_401_UNAUTHORIZED
 
 from yama.user._service import UserDb
-from yama.user._service_password import is_password_valid
+from yama.user._service_password import (
+    hash_password,
+    is_password_valid,
+    should_rehash_password_with_hash,
+)
 from yama.user.auth import Config
 from yama.user.auth._router import PasswordGrantIn, _TokenOut
 from yama.user.auth._service_token_access import make_access_token_and_expires_in
@@ -37,6 +41,21 @@ async def password_grant_in_to_token_out(
         or not is_password_valid(password_grant_in.password, user_db.password_hash)
     ):
         raise InvalidUsernameOrPasswordError()
+
+    if should_rehash_password_with_hash(user_db.password_hash):
+        update_password_hash = hash_password(password_grant_in.password)
+        update_query = (
+            update(UserDb)
+            .values(
+                password_hash=update_password_hash,
+            )
+            .where(UserDb.id == user_db.id)
+            .returning(UserDb)
+        )
+        update_row = (await connection.execute(update_query)).mappings().one_or_none()
+        if update_row is None:
+            ...  # TODO: Log
+        await connection.commit()
 
     access_token, expires_in = make_access_token_and_expires_in(
         user_db.id, settings=settings
