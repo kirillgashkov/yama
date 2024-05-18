f6bb55d5fe89963e06ae2667efcc38621af0b2b5
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-05-18 18:18:56 +0300

    Clean up database

--- backend/yama/database/__init__.py
+++ backend/yama/database/__init__.py
@@ -1,5 +1,5 @@
 from ._config import Config as Config
-from ._database import BaseTable as BaseTable
-from ._database import get_connection as get_connection
-from ._database import make_connection as make_connection
-from ._database import make_engine as make_engine
+from ._service import BaseTable as BaseTable
+from ._service import get_connection as get_connection
+from ._service import make_connection as make_connection
+from ._service import make_engine as make_engine
