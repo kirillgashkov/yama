762c19d62041dc08ea176ca5867c17fb1efeb4fc
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-07 10:41:26 +0300

    Remove function implementation

--- backend/yama/function/driver/_config.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix="yama__function__driver__")
-
-    helper_executable: list[str]
--- backend/yama/function/driver/helper/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._service import FileInout as FileInout
-from ._service import HelperIn as HelperIn
-from ._service import HelperOut as HelperOut
