68a27934a22f01f3d2a95639835004abbbd1ade8
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-09 10:19:38 +0300

    Add Pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
 import "@/style.css";
 import App from "@/App.vue";
 import router from "@/router";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+const pinia = createPinia();
+
 const app = createApp(App);
+app.use(pinia);
 app.use(router);
 app.mount("#app");
