3039ff3d454f1b6d334bd8088ca5e2d0e844e5c7
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-16 16:38:02 +0300

    Add the editor page template base

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,6 +3,14 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
   { path: "/auth", component: () => import("./components/AuthPage.vue") },
   {
+    path: "/files",
+    redirect: "/files/",
+  },
+  {
+    path: "/files/:path(.*)",
+    component: () => import("./components/EditorPageTemplate.vue"),
+  },
+  {
     path: "/:pathMatch(.*)",
     component: () => import("./components/NotFoundPage.vue"),
   },
