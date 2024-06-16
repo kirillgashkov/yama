f6e95c592c5d4452ef591ac47d7321498850d57f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-16 16:42:51 +0300

    Add basic file metadata fetch

--- frontend/src/components/EditorPageTemplate.vue
+++ frontend/src/components/EditorPageTemplate.vue
@@ -1,3 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useApiService } from "@/api/service";
 
-<template>Editor.</template>
+const api = useApiService();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const file: any = ref(null);
+
+async function getFile() {
+  try {
+    file.value = await api.get("/files/etc/passwd");
+  } catch (error) {
+    file.value = null;
+  }
+}
+</script>
+
+<template>
+  <p><button @click="getFile">Get file</button></p>
+  <p>file: {{ file || "null" }}</p>
+</template>
