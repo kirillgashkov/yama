7d65bfe6efed0f2dbd47cf170d24def1894d9ed1
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-16 16:59:05 +0300

    Add basic content fetch

--- frontend/src/components/EditorPageTemplate.vue
+++ frontend/src/components/EditorPageTemplate.vue
@@ -7,6 +7,9 @@ const api = useApiService();
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const file: any = ref(null);
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const content: any = ref(null);
+
 async function getFile() {
   try {
     file.value = await api.get("/files/etc/passwd");
@@ -14,9 +17,19 @@ async function getFile() {
     file.value = null;
   }
 }
+
+async function getContent() {
+  try {
+    content.value = await api.get(file.value.content.url);
+  } catch (error) {
+    content.value = null;
+  }
+}
 </script>
 
 <template>
   <p><button @click="getFile">Get file</button></p>
   <p>file: {{ file || "null" }}</p>
+  <p><button @click="getContent">Get content</button></p>
+  <p>content: {{ content || "null" }}</p>
 </template>
