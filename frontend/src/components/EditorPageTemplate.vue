f302cdefa8f858083991f560fc2bc318ae899fcf
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-17 01:24:44 +0300

    Draft CodeMirror in editor

--- frontend/src/components/EditorPageTemplate.vue
+++ frontend/src/components/EditorPageTemplate.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { type Ref, ref } from "vue";
+import { type Ref, ref, shallowRef } from "vue";
 import { useApiService } from "@/api/service";
+import { Codemirror } from "vue-codemirror";
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 
 const api = useApiService();
 
 const file: Ref<unknown> = ref(null);
-
-const content: Ref<unknown> = ref(null);
 
 async function getFile() {
   try {
@@ -16,13 +16,39 @@ async function getFile() {
   }
 }
 
+const content: Ref<string> = ref("");
+
 async function getContent() {
   try {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const r = await api.getAsResponse((file.value as any).content.url);
     content.value = await r.text();
   } catch (error) {
-    content.value = null;
+    content.value = "";
   }
+}
+
+const extensions = [markdown({ base: markdownLanguage })];
+const view = shallowRef();
+
+function handleReady(payload: {
+  view: import("@codemirror/view").EditorView;
+}): boolean {
+  view.value = payload.view;
+  return true;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getCodemirrorStates() {
+  const state = view.value.state;
+  const ranges = state.selection.ranges;
+
+  // const selected = ranges.reduce((r, range) => r + range.to - range.from, 0);
+  const cursor = ranges[0].anchor;
+  const length = state.doc.length;
+  const lines = state.doc.lines;
+
+  return { cursor, length, lines };
 }
 </script>
 
@@ -31,4 +57,19 @@ async function getContent() {
   <p>file: {{ file || "null" }}</p>
   <p><button @click="getContent">Get content</button></p>
   <p>content: {{ content || "null" }}</p>
+  <div>
+    <codemirror
+      v-model="content"
+      placeholder="Code goes here..."
+      :style="{ height: '400px' }"
+      :autofocus="true"
+      :indent-with-tab="true"
+      :tab-size="2"
+      :extensions="extensions"
+      @ready="handleReady"
+      @change="console.log('change', $event)"
+      @focus="console.log('focus', $event)"
+      @blur="console.log('blur', $event)"
+    />
+  </div>
 </template>
