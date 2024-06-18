4f88428917db7f6c13490211cec1de679eae2740
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-18 11:15:06 +0300

    Add path and workingFileId properties to editor

--- frontend/src/components/EditorPageTemplate.vue
+++ frontend/src/components/EditorPageTemplate.vue
@@ -1,3 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type Ref, type ComputedRef, ref, computed } from "vue";
+import { useRoute } from "vue-router";
 
-<template>Editor.</template>
+const route = useRoute();
+
+const path: Ref<string> = ref(<string>route.params.path);
+const workingFileId: ComputedRef<string> = computed(() => {
+  const idOrIds = route.query.working_file_id;
+
+  if (Array.isArray(idOrIds)) {
+    return idOrIds.pop() ?? "";
+  }
+
+  return idOrIds ?? "";
+});
+</script>
+
+<template>
+  <h1>{{ path }}</h1>
+  <h1>{{ workingFileId }}</h1>
+  <button @click="console.log(workingFileId)">Button</button>
+</template>
