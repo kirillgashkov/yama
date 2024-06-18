66248105d1ed7b7a20df1f4c976fc581036cd420
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-18 12:49:39 +0300

    Add MarkdownDiv

---
+++ frontend/src/components/MarkdownDiv.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, type ComputedRef } from "vue";
+import type Token from "markdown-it/lib/token.mjs";
+
+import { renderSanitize } from "@/markdown";
+
+const props = defineProps<{
+  content: string | Token[];
+}>();
+
+const innerHtml: ComputedRef<string> = computed(() => {
+  return renderSanitize(props.content);
+});
+</script>
+
+<template>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-html="innerHtml"></div>
+</template>
