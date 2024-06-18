8f33a170b0c6634a6ff3b2cf6cef15ce995ea10c
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-06-18 11:14:52 +0300

    Clean up routes

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -11,7 +11,7 @@ const routes = [
     component: () => import("./components/EditorPageTemplate.vue"),
   },
   {
-    path: "/:pathMatch(.*)",
+    path: "/:path(.*)",
     component: () => import("./components/NotFoundPageTemplate.vue"),
   },
 ];
